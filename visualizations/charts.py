21228f6f91fb7347cc883b1c69abd86d7944bdb0
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-02-12 14:11:01 +0200

    Added authors chart to charts.py

--- visualizations/charts.py
+++ visualizations/charts.py
@@ -1,2 +1,8 @@
 # Visualization and chart creation functions
 # Functions to create various chart types using Plotly (bar charts, line charts, pie charts, etc.)
+
+import plotly.express as px
+from sections.dataframes import top_10_authors
+
+def top_authors_chart():
+    return px.bar(top_10_authors(), x="Authors", y="Count")
