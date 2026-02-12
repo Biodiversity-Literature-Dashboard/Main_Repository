67b5bf34b7b95d3155d8ef6ea8872b264cdc228a
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-02-12 14:10:05 +0200

    Commented away imports inside __init__ files until functions with such names are made

--- sections/__init__.py
+++ sections/__init__.py
@@ -3,8 +3,8 @@ Sections Package
 Dashboard sections for different views.
 """
 
-from .overview import create_overview_section
-from .data_section import create_data_section
+#from .overview import create_overview_section
+#from .data_section import create_data_section
 
 __all__ = [
     'create_overview_section',
