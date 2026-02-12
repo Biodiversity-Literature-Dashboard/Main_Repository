c5a562e13982e3984c06a97ce226025c56b9a778
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-02-12 14:10:41 +0200

    Commented away old app layout

--- app.py
+++ app.py
@@ -4,43 +4,38 @@ from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
-from backend.src.tables import top_10_authors
+from visualizations.charts import top_authors_chart
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = dbc.Container([
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    "Dashboard"
-                ])
-            ])
-        ])
-    ])
-])
-
-if __name__ == "__main__":
-    print("Starting Dashboard...")
-    app.run(debug=True)
-
-app = Dash()
-top_10_authors = top_10_authors()
-
-authors_fig = px.bar(top_10_authors, x="Authors", y="Count")
 
 app.layout = html.Div(children=[
     html.H1(children='Biodiversity Dashboard'),
 
     html.Div(children='''
-        Authors who have written the most articles.
+        Authors who have listed the most threats.
     '''),
 
     dcc.Graph(
         id='top_authors_graph',
-        figure=authors_fig
+        figure=top_authors_chart()
     )
 ])
 
-if __name__ == '__main__':
+# app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# app.layout = dbc.Container([
+#     dbc.Row([
+#         dbc.Col([
+#             dbc.Card([
+#                 dbc.CardBody([
+#                     "Dashboard"
+#                 ])
+#             ])
+#         ])
+#     ])
+# ])
+
+if __name__ == "__main__":
+    print("Starting Dashboard...")
     app.run(debug=True)
