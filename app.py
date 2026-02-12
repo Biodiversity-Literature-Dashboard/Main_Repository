b81d9d39411a8653f5edf3eda06f718426cb69fb
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-02-12 13:36:19 +0200

    removed frontend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # Main Dash application entry point
 # This file initializes the Dash app and runs the server
-
-from dash import Dash
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+from backend.src.tables import top_10_authors
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -20,4 +22,25 @@ app.layout = dbc.Container([
 
 if __name__ == "__main__":
     print("Starting Dashboard...")
+    app.run(debug=True)
+
+app = Dash()
+top_10_authors = top_10_authors()
+
+authors_fig = px.bar(top_10_authors, x="Authors", y="Count")
+
+app.layout = html.Div(children=[
+    html.H1(children='Biodiversity Dashboard'),
+
+    html.Div(children='''
+        Authors who have written the most articles.
+    '''),
+
+    dcc.Graph(
+        id='top_authors_graph',
+        figure=authors_fig
+    )
+])
+
+if __name__ == '__main__':
     app.run(debug=True)
