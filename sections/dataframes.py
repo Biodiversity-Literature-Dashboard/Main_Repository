b7dafb0741c3420fd93326779bd825d5cafaebbc
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-02-19 14:50:21 +0200

    added pandas of ridley bibliography dataframe with cleaned authors

--- sections/dataframes.py
+++ sections/dataframes.py
@@ -1,8 +1,12 @@
 import pandas as pd
-from utils.data_loader import df1,df2
+from utils.data_loader import df1,df2, df_ridley_bib
 
 def get_authors(df):
     return df['Authors']
+
+def clean_authors(df):
+    cleaned_df = df.replace({"_etal": " et al."},regex=True)
+    return cleaned_df
 
 def count_values(row):
     return row.value_counts()
@@ -16,5 +20,12 @@ def top_10_authors():
     top_authors = get_top_10(authors_counted)
     return pd.DataFrame({'Authors':top_authors.index, 'Count':top_authors.values})
 
+def create_ridley_bib_table():
+    df= df_ridley_bib[['Authors','Year','Title']]
+    df =clean_authors(df)
+    return df
+    
 
-#print(top_10_authors())
+
+if __name__ == "__main__":
+    print(create_ridley_bib_table())
