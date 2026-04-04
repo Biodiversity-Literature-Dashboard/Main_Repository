c4fd12572aba7dc1b1e32a8d268dbaad302559b3
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-04-04 03:38:15 +0300

    cut extract threat category from code into smaller parts

--- utils/logic/filters.py
+++ utils/logic/filters.py
@@ -15,6 +15,7 @@ def ecoregion_filter(df, ecoregions):
 
 def study_design_filter(df, study_designs):
     if not column_exists(df, 'Study_design') or not apply_filter(study_designs):
+        print(study_designs)
         return df
     return df[df['Study_design'].isin(study_designs)]
 
@@ -27,30 +28,43 @@ def threat_category_filter(df, threat_category):
     return df[threat_mask]
 
 def column_exists(df,column):
-    column_exists = column in df.columns
-    if not column_exists:
+    c_exists = column in df.columns
+    if not c_exists:
         print(f'Error: {column} column missing or not found')
-    return column_exists
+    return c_exists
 
 def apply_filter(df_filter):
-    #check if filter should be applied
+    """check if filter should be applied"""
     return df_filter and len(df_filter) > 0
 
-def extract_threat_category_from_code(threat_code):
+def extract_threat_category_from_code(threat_codes):
     """
     Extract main category number from threat code.
     Example: '6.3:Other' -> '6', '2.1:AgNTC;9.5:PollAir' -> ['2', '9']
     """
-    if pd.isna(threat_code):
+    if pd.isna(threat_codes):
         return []
-    
+    codes = split_threat_codes(threat_codes)
+    categories =extract_codes(codes)
+    return categories
+
+def split_threat_codes(threat_codes):
+    """Split by semicolon for multiple threats"""
+    return str(threat_codes).split(';')
+
+def extract_codes(codes):
+    """For loop for extracting threat categories"""
     categories = set()
-    # Split by semicolon for multiple threats
-    codes = str(threat_code).split(';')
     for code in codes:
-        # Extract number before the dot
-        if '.' in code:
-            category = code.split('.')[0].strip()
-            categories.add(category)
-    
+        categories = number_before_dot(code,categories)
+    return to_sorted_list(categories)
+
+def to_sorted_list(categories):
     return sorted(list(categories))
+
+def number_before_dot(code,categories):
+    """ Extract number before the dot and add it to categories"""
+    if '.' in code:
+        category = code.split('.')[0].strip()
+        categories.add(category)
+    return categories
